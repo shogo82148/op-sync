ccd83b06a5a69d29bef28f68c0e09c57563012f4
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-18 23:10:33 +0900

    basic implement of inject type

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/shogo82148/op-sync
 
 go 1.21.3
+
+require github.com/goccy/go-yaml v1.11.2
+
+require (
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,67 @@
 package main
 
-func main() {
+import (
+	"context"
+	"os"
+	"os/exec"
+	"strings"
 
+	"github.com/goccy/go-yaml"
+)
+
+func main() {
+	data, err := os.ReadFile(".op-sync.yml")
+	if err != nil {
+		panic(err)
+	}
+
+	var config Config
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		panic(err)
+	}
+
+	if err := run(&config); err != nil {
+		panic(err)
+	}
+}
+
+type Config struct {
+	Secrets []*SyncConfig `yaml:"secrets"`
+}
+
+type SyncConfig struct {
+	Type SyncType `yaml:"type"`
+
+	// for SyncTypeInject
+	Output   string `yaml:"output"`   // output file path
+	Template string `yaml:"template"` // the template
+}
+
+type SyncType string
+
+const (
+	// SyncTypeInject is the type for injecting secrets into a file.
+	SyncTypeInject SyncType = "inject"
+)
+
+func run(cfg *Config) error {
+	for _, secret := range cfg.Secrets {
+		switch secret.Type {
+		case SyncTypeInject:
+			if err := runInject(context.TODO(), secret); err != nil {
+				return err
+			}
+		default:
+		}
+	}
+	return nil
+}
+
+func runInject(ctx context.Context, cfg *SyncConfig) error {
+	cmd := exec.CommandContext(ctx, "op", "inject", "--force", "--out-file", cfg.Output)
+	cmd.Stdin = strings.NewReader(cfg.Template)
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
 }
