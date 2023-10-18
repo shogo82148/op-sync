483ddde3cb548b49475a47460eae070952ed5408
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-18 23:49:23 +0900

    separate package

---
+++ internal/opsync/op-sync.go
@@ -0,0 +1,68 @@
+package opsync
+
+import (
+	"context"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/goccy/go-yaml"
+)
+
+type Config struct {
+	Secrets []*SyncConfig `yaml:"secrets"`
+}
+
+type SyncConfig struct {
+	Type SyncType `yaml:"type"`
+
+	// for SyncTypeInject
+	Output   string `yaml:"output"`   // output file path
+	Template string `yaml:"template"` // the template
+}
+
+type SyncType string
+
+const (
+	// SyncTypeInject is the type for injecting secrets into a file.
+	SyncTypeInject SyncType = "inject"
+)
+
+func Run(ctx context.Context, args []string) int {
+	data, err := os.ReadFile(".op-sync.yml")
+	if err != nil {
+		panic(err)
+	}
+
+	var config Config
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		panic(err)
+	}
+
+	if err := run(&config); err != nil {
+		panic(err)
+	}
+	return 0
+}
+
+func run(cfg *Config) error {
+	for _, secret := range cfg.Secrets {
+		switch secret.Type {
+		case SyncTypeInject:
+			if err := runInject(context.TODO(), secret); err != nil {
+				return err
+			}
+		default:
+		}
+	}
+	return nil
+}
+
+func runInject(ctx context.Context, cfg *SyncConfig) error {
+	cmd := exec.CommandContext(ctx, "op", "inject", "--force", "--out-file", cfg.Output)
+	cmd.Stdin = strings.NewReader(cfg.Template)
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -3,65 +3,14 @@ package main
 import (
 	"context"
 	"os"
-	"os/exec"
-	"strings"
+	"os/signal"
 
-	"github.com/goccy/go-yaml"
+	"github.com/shogo82148/op-sync/internal/opsync"
 )
 
 func main() {
-	data, err := os.ReadFile(".op-sync.yml")
-	if err != nil {
-		panic(err)
-	}
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
 
-	var config Config
-	if err := yaml.Unmarshal(data, &config); err != nil {
-		panic(err)
-	}
-
-	if err := run(&config); err != nil {
-		panic(err)
-	}
-}
-
-type Config struct {
-	Secrets []*SyncConfig `yaml:"secrets"`
-}
-
-type SyncConfig struct {
-	Type SyncType `yaml:"type"`
-
-	// for SyncTypeInject
-	Output   string `yaml:"output"`   // output file path
-	Template string `yaml:"template"` // the template
-}
-
-type SyncType string
-
-const (
-	// SyncTypeInject is the type for injecting secrets into a file.
-	SyncTypeInject SyncType = "inject"
-)
-
-func run(cfg *Config) error {
-	for _, secret := range cfg.Secrets {
-		switch secret.Type {
-		case SyncTypeInject:
-			if err := runInject(context.TODO(), secret); err != nil {
-				return err
-			}
-		default:
-		}
-	}
-	return nil
-}
-
-func runInject(ctx context.Context, cfg *SyncConfig) error {
-	cmd := exec.CommandContext(ctx, "op", "inject", "--force", "--out-file", cfg.Output)
-	cmd.Stdin = strings.NewReader(cfg.Template)
-	if err := cmd.Run(); err != nil {
-		return err
-	}
-	return nil
+	os.Exit(opsync.Run(ctx, os.Args[1:]))
 }
